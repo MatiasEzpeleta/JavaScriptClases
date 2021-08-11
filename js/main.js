7077dd58757d0f5532fab07426a6e29287e799c6
Matias <matiasezpeleta@gmail.com>
2021-08-11 20:34:16 -0300

    Desafio funciones

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+let free = false;
 
-let numero = prompt("ingresar un numero");
+const validarCliente = (time) =>{
+    let edad = Number( prompt("¿Cual es tu edad?"));
+    if (edad > 18) {
+       if (time >= 2 && time < 7 && free==false) {
+           alert("Despues de las 2 entrada gratis");
+           free = true;
 
-while (numero <= 100) {
-    numero++;
-    document.write(numero + "<br>");
-    if(numero <= 10) {
-        continue;
-    }
-
-    if(numero == 20) {
-        break;
+       } else {
+           alert(`Son las ${time}:00hs y podes pasar, pero tenes que pagar la entrada`);
+       }
+    } else {
+        alert("Solo para mayores de 18 años")
     }
 }
+
+validarCliente(23)
+validarCliente(00)
+validarCliente(1)
+validarCliente(2)
+validarCliente(3)
+validarCliente(4)
+validarCliente(5)
+validarCliente(6)
+
+
+
 
 
